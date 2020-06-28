f485203d34e23bd37522261a4f58cbbf62631835
Sona337 <67533264+Sona337@users.noreply.github.com>
2020-06-28 12:28:54 +0530

    created size.cpp

---
+++ Size.cpp
@@ -0,0 +1,14 @@
+//Enter program title here
+#include<conio.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ 	 cout<<"Size of int is "<<sizeof(int)<<"bytes";
+	 cout<<"\nSize of char is "<<sizeof(char)<<" bytes";
+ 	 cout<<"\nSize of float is "<<sizeof(float)<<" bytes";
+ 	 cout<<"\nSize of double is "<<sizeof(double)<<" bytes";
+ 	 
+ 	 return 0;
+}
